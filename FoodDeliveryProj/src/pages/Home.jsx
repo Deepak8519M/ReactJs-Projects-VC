cbe6e59c3ec8b4d8fc330fda3279034bc407f183
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-13 12:06:07 +0530

    few changes

--- FoodDeliveryProj/src/pages/Home.jsx
+++ FoodDeliveryProj/src/pages/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
   return (
     <div className="w-full min-h-screen bg-slate-200">
       <Nav />
-      <div className="flex flex-wrap justify-center items-center gap-4 bg-red-300 w-full">
+      <div className="flex flex-wrap justify-center items-center gap-4  w-full cursor-pointer">
         {Categories.map((item) => {
           return (
-            <div className="w-[100px] h-[100px] bg-white flex items-center gap-2 p-4 justify-center flex-col">
+            <div className="w-[120px] h-[120px] bg-white flex items-center gap-2 p-4 justify-center flex-col text-[16px] font-semibold text-gray-600 rounded-sm shadow-xl hover:bg-green-200 transition-all duration-200 hover:text-black ">
               <div>{item.icon}</div>
               <div>{item.name}</div>
             </div>
