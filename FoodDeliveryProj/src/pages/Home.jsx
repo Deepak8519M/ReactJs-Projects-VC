9ec33f5dbb87aa91f2bf3392e0924af0235e8a49
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-16 11:27:28 +0530

    code written

--- FoodDeliveryProj/src/pages/Home.jsx
+++ FoodDeliveryProj/src/pages/Home.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../components/Nav";
 import Categories from "../Category";
 import Card from "../components/Card";
 import { food_items } from "../food";
 
 function Home() {
+  let [cate, setCate] = useState(food_items);
+
+  function filter(category) {
+    if (category === "All") {
+      setCate(food_items);
+    } else {
+      let newList = food_items.filter(
+        (item) => item.food_category === category
+      );
+      setCate(newList);
+    }
+  }
   return (
     <div className="w-full min-h-screen bg-slate-200">
       <Nav />
       <div className="flex flex-wrap justify-center items-center gap-4  w-full cursor-pointer">
         {Categories.map((item) => {
           return (
-            <div className="w-[120px] h-[120px] bg-white flex items-center gap-2 p-4 justify-center flex-col text-[16px] font-semibold text-gray-600 rounded-sm shadow-xl hover:bg-green-200 transition-all duration-200 hover:text-black ">
+            <div
+              className="w-[120px] h-[120px] bg-white flex items-center gap-2 p-4 justify-center flex-col text-[16px] font-semibold text-gray-600 rounded-sm shadow-xl hover:bg-green-200 transition-all duration-200 hover:text-black"
+              onClick={() => filter(item.name)}
+            >
               <div>{item.icon}</div>
               <div>{item.name}</div>
             </div>
@@ -19,7 +34,7 @@ function Home() {
         })}
       </div>
       <div className="w-full flex-wrap gap-5 px-5 pt-8 pb-8 flex justify-center items-center">
-        {food_items.map((item) => (
+        {cate.map((item) => (
           <Card
             name={item.food_name}
             image={item.food_image}
